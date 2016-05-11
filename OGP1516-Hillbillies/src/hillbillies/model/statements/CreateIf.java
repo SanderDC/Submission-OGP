430b38da9db9ccf85016c0ab8d6b1cc31e734747
bram <bram@10.43.114.186>
2016-05-11 11:52:41 +0200

    i push this stuff in your shoes

--- OGP1516-Hillbillies/src/hillbillies/model/statements/CreateIf.java
+++ OGP1516-Hillbillies/src/hillbillies/model/statements/CreateIf.java
@@ -1,23 +1,24 @@
 package hillbillies.model.statements;
 
 import hillbillies.model.Task;
+import hillbillies.model.expressions.BooleanExpression;
 import hillbillies.model.expressions.Expression;
 
 public class CreateIf extends Statement {
-	public  CreateIf(Expression expression, Statement lStatement, Statement rStatement) {
+	public  CreateIf(BooleanExpression expression, Statement lStatement, Statement rStatement) {
 		this.expression=expression;
 		this.trueStatement=lStatement;
 		this.falseStatement=rStatement;
 	}
 		
 	
-		private Expression expression;
+		private BooleanExpression expression;
 		private Statement trueStatement;
 		private Statement falseStatement;
 		
 		@Override
 		public void execute(){
-			if (expression.evaluate) {
+			if (expression.evaluate()) {
 				trueStatement.execute();
 			}
 			else {
--- OGP1516-Hillbillies/src/hillbillies/model/statements/CreateWhile.java
+++ OGP1516-Hillbillies/src/hillbillies/model/statements/CreateWhile.java
@@ -1,19 +1,20 @@
 package hillbillies.model.statements;
 
 import hillbillies.model.Task;
+import hillbillies.model.expressions.BooleanExpression;
 import hillbillies.model.expressions.Expression;
 
 public class CreateWhile extends Statement {
- public CreateWhile(Expression condition, Statement body){
+ public CreateWhile(BooleanExpression condition, Statement body){
 	 this.expression=condition;
 	 this.body=body;
  }
-private Expression expression;
+private BooleanExpression expression;
 private Statement body;
  
  @Override
  public void execute() {
-	 while(expression.evaluate){
+	 while(expression.evaluate()){
 		 body.execute();
 	 }
 		 	
